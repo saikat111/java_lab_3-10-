7b5bc3d04b131535e416ecc29504e72ad74fca27
al rakib sorder <46457169+saikat111@users.noreply.github.com>
2019-10-03 11:48:07 +0600

    Add files via upload

---
+++ Overrider.java
@@ -0,0 +1,38 @@
+class A{
+	int i,j;
+	A(int i,int j){
+	this.i=i;
+	this.j=j;
+	}
+
+
+
+void show(){
+	System.out.println("i and j: " + i + " " + j);
+	}
+}
+
+class B extends A{
+
+	int k;
+	B(int i,int j,int k){
+	super(i,j);
+	this.k=k;
+}
+void show(){
+	super.show();
+	System.out.println("k" + k);
+	}
+}
+
+class Overrider{
+	public static void main(String args[]){
+	B myb = new B(5,10,15);
+	myb.show();
+	}
+}
+	
+	
+
+
+	
